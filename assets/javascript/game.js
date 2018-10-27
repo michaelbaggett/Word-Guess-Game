bfdf4c7ea0e3a537f6daa19d920bacd038a961f1
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-27 15:45:14 -0500

    added bare html info and beginning pseudo code for JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+//variables:
+
+    //directions of press any letter to play
+    //array of words
+    //blanks representing letters in selected word
+    //if user guesses corect letter, replace space with letter
+    //letters which user as guessed
+    //# of guesses remaining
+    //wins
+    //losses
+
+
+//function definitions
+    //random word guess function
+    //reset game function
+    //select element function
+
+
+//click/key handlers
+    //onkeyup save userGuess
+    //compare user guess to letters in random word
+    //if letter matches letter in word, replace space with letter
+    //if letter doesnt match, amount of guesses remaining goes down by 1
+    //if win or lose, reset game
