06fe7b0e1658e2ea4c3cb132a23d57c79e193731
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-27 17:03:56 -0500

    user guesses now added into guess log array and displayed on page, need to make showing the letter math how hw instructions indicate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+//on window load, begin script
+window.onload = function() {
+
 //variables:
 
     //directions of press any letter to play
@@ -5,11 +8,12 @@
     //blanks representing letters in selected word
     //if user guesses corect letter, replace space with letter
     var wordList = [];
-    var lettersGuessed = [ ];
+    var guessLog = [];
     var guessesLeft = 10;
     var wins = 0;
     var losses = 0;
     
+    var lettersGuessed = document.getElementById("you-guessed");
 
 
 //function definitions
@@ -29,9 +33,13 @@
     //onkeyup save userGuess
     document.onkeyup = function(event){
         userGuess = event.key.toUpperCase();
-        console.log(userGuess);
-    };
+        //console.log(userGuess);
+        guessLog.push(userGuess);
+    //display user letter guess to page
+    lettersGuessed.innerHTML = ("You have guessed: " + guessLog);
     //compare user guess to letters in random word
     //if letter matches letter in word, replace space with letter
     //if letter doesnt match, amount of guesses remaining goes down by 1
     //if win or lose, reset game
+};
+}
