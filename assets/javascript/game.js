f8667ee61420870f7b129d239c92193de7a1568c
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-27 16:08:08 -0500

    added variables, reset function, as well as random word generator function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,25 @@
     //array of words
     //blanks representing letters in selected word
     //if user guesses corect letter, replace space with letter
-    //letters which user as guessed
-    //# of guesses remaining
-    //wins
-    //losses
+    var wordList [] 
+    var lettersGuessed = [ ]
+    var guessesLeft = 10
+    var wins = 0
+    var losses = 0
+    
 
 
 //function definitions
-    //random word guess function
-    //reset game function
-    //select element function
+    //this will generate our random word
+    var randomWord = wordList[Math.floor(Math.random()*wordList.length)];
+    //this will reset our game
+    function reset(){
+        randomLetter = wordList[Math.floor(Math.random()*wordList.length)];
+        //console.log(randomWord);
+        guessesLeft = 10;
+        lettersGuessed.length = 0;
+    }
+
 
 
 //click/key handlers
