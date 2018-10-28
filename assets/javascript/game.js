7ad37c257e65ab00f61786df4aa61b10620987a3
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-10-28 14:16:43 -0500

    functions created to check guess, check win/loss and update page accordingly, does not work, following error received game.js:51 Uncaught TypeError: Cannot set property 'innerHTML' of null     at updateDisplay (game.js:51)     at makeGuess (game.js:97)     at HTMLDocument.document.onkeyup (game.js:109)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,19 +20,18 @@ window.onload = function() {
     var wins = 0;   //number of wins
     var losses = 0; //number of losses
     var guessingWord = [];  //array in which we build the word to match computer word
-    
-    var lettersGuessed = document.getElementById("you-guessed");
-    var guessAmount = document.getElementById("numOfGuessLeft");
+    var hasFinished = false
 
 
 //function definitions
     //this will generate our random word
     var randomWord = wordList[Math.floor(Math.random()*wordList.length)];
     //this will reset our game
-    function reset(){
+    function resetGame(){
         remainingGuesses = maxGuesses; //resets guess amount
         //select a random word
-        randomLetter = wordList[Math.floor(Math.random()*wordList.length)];
+        randomWord = wordList[Math.floor(Math.random()*wordList.length)];
+        console.log(randomWord);
         //clear out letter guesses & our guesses against word
         guessLog = [];
         guessingWord = [];
@@ -42,28 +41,76 @@ window.onload = function() {
         updateDisplay();
     };
 
+    function updateDisplay(){
+        document.getElementById("win-count").innerHTML = ("Wins: " + wins);
+        document.getElementById("loss-count").innerHTML = ("Losses: " + losses);
+        document.getElementById("comp-word").innerHTML = "";
+        for (var i = 0; i < guessingWord.length; i++) {
+            document.getElementById("comp-word").innerHTML += guessingWord[i];
+        };
+        document.getElementById("numOfGuessesLeft").innerHTML = remainingGuesses;
+        document.getElementById("you-guessed").innerHTML = guessLog;
+        if (remainingGuesses <= 0){
+            hasFinished = true;
+        }
+    };
+
+    function evaluateGuess(letter) {
+        var positions = [];
+
+        for (var i = 0; i < wordList[wordIndex].length; i++){
+            if(wordList[wordIndex][i] === letter) {
+                positions.push(i);
+         }
+    }
+        if (positions.length <= 0){
+        remainingGuesses--;
+        } else {
+            for(var i = 0; i < positions.length; i++){
+            guessingWord[positions[i]] = letter;
+        }
+    }
+};
+
+    function checkWin(){
+        if(guessingWord.indexOf("_") === -1) {
+            wins++;
+            hasFinished = true;
+            alert("Nice Job!")
+        }
+    };
+
+    function checkLoss(){
+        if(remainingGuesses <= 0) {
+            hasFinished = true;
+            alert("You Lose!")
+        }
+    };
+
+    function makeGuess(letter){
+        if(remainingGuesses > 0) {
+            if (guessLog.indexOf(letter)=== -1){
+                guessLog.push(letter);
+            evaluateGuess(letter);
+                }
+            }
+            updateDisplay();
+            checkWin();
+        };
 
 
 //click/key handlers
     //onkeyup save userGuess
     document.onkeyup = function(event){
-        if (event.keyCode >= 65 && event.keyCode <=90){
-        userGuess = event.key.toUpperCase();
-        //console.log(userGuess);
-        guessLog.push(userGuess);
-        }
+        if(hasFinished){
+            resetGame();
+            hasFinished = false;
+        } else if (event.keyCode >= 65 && event.keyCode <=90) {
+        makeGuess(event.key.toLocaleUpperCase());
+        updateDisplay();
+        checkWin();
+        };
 
-        // if (userGuess !== wordList) {
-        //     guessesLeft--;
-        // } else if (guessesLeft === 0) {
-        //     alert("You Lose!");
-        // }
-    //display user letter guess to page
-    lettersGuessed.innerHTML = ("You have guessed: " + guessLog);
-    guessAmount.innerHTML = ("You have " + guessesLeft + " guesses remaining");
-    //compare user guess to letters in random word
-    //if letter matches letter in word, replace space with letter
-    //if letter doesnt match, amount of guesses remaining goes down by 1
-    //if win or lose, reset game
-};
+
 }
+};
